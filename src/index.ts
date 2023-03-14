3f434b0ab999fcf21152fef6a7065824cb44bc60
xie <709955748@qq.com>
2023-03-14 14:40:14 +0800

    build

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import {LitElement, css, html} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+
+@customElement('simple-greeting')
+export class SimpleGreeting extends LitElement {
+  static styles = css`
+    :host {
+      color: blue;
+    }
+  `;
+
+  @property()
+  name?: string = 'World';
+
+  render() {
+    return html`<p>Hello, ${this.name}!</p>`;
+  }
+}
+
+let test = new SimpleGreeting();
+test.name = "test";
+document.body.appendChild(test);
